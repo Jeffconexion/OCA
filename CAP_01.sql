5e63d436e5cda8eeb6752c82946e2778909e0bf9
Jeffconexion <jeff_conexion2014@yahoo.com>
2018-09-21 18:19:24 -0300

    Capitulo 01 finalizado - foi abordado vários assuntos como ddl dml tcl e as coisas que serão cobradas na certificação.

--- CAP_01.sql
+++ CAP_01.sql
@@ -7,3 +7,17 @@
 /*|  Info.......: No capítulo 1 mostrou uma abordagem                 |*/
 /*|               geral do que será estudado no livro.                |*/
 /*[]-----------------------------------------------------------------[]*/
+
+-- CRIANDO TABELA
+CREATE TABLE NAVIOS(
+  NAVIO_ID NUMBER,
+  NAVIO_NOME VARCHAR2(20),
+  CAPACIDADE NUMBER,
+  COMPRIMENTO NUMBER
+);
+
+-- INSERINDO
+INSERT INTO NAVIOS(NAVIO_ID,NAVIO_NOME,CAPACIDADE,COMPRIMENTO)VALUES(1, 'CODD CRISTAL',2052, 855);
+
+-- CRIANDO UMA CONSULTA
+SELECT NAVIO_NOME,CAPACIDADE,COMPRIMENTO FROM NAVIOS;
