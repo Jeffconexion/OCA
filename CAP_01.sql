5eb017188b7f6e3553b2b760315b6041b7fea91e
Jeffconexion <jeff_conexion2014@yahoo.com>
2018-09-21 17:17:26 -0300

    Cap 01 Oracle and Structured Query Language.

---
+++ CAP_01.sql
@@ -0,0 +1,9 @@
+/*[]-----------------------------------------------------------------[]*/
+/*|                     CERTIFICAÇÃO OCA                              |*/
+/*|*******************************************************************|*/
+/*|  Book.......: OCA Oracle database       Data..: 21.09.2018	      |*/
+/*|  Cap_01.....: Oracle	and	Structured	Query	Language (SQL)        |*/
+/*|  @Author....: Jefferson                                           |*/
+/*|  Info.......: No capítulo 1 mostrou uma abordagem                 |*/
+/*|               geral do que será estudado no livro.                |*/
+/*[]-----------------------------------------------------------------[]*/
